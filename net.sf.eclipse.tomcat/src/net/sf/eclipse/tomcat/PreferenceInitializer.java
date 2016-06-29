51466c4944e6d38fcd84faef83fe544a7f9f44c9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-06-30 01:00:07 +0200

    removed empty public constructor

--- net.sf.eclipse.tomcat/src/net/sf/eclipse/tomcat/PreferenceInitializer.java
+++ net.sf.eclipse.tomcat/src/net/sf/eclipse/tomcat/PreferenceInitializer.java
@@ -5,11 +5,6 @@ import org.eclipse.jface.preference.IPreferenceStore;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
-	public PreferenceInitializer() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
 	public void initializeDefaultPreferences() {
 		IPreferenceStore prefStore = TomcatLauncherPlugin.getDefault().getPreferenceStore();
 		prefStore.setDefault(TomcatLauncherPlugin.TOMCAT_PREF_CONFMODE_KEY, TomcatLauncherPlugin.CONTEXTFILES_MODE);		
